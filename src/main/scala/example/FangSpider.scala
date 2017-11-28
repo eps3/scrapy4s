2b71b8d1f5b994ef641ddf7d2562bf268cbaae01
sheepmen <747033643@qq.com>
2017-11-28 18:10:26 +0800

    dev: update fang spider

--- src/main/scala/example/FangSpider.scala
+++ src/main/scala/example/FangSpider.scala
@@ -10,10 +10,13 @@ import spider.Spider
   */
 object FangSpider {
   def main(args: Array[String]): Unit = {
-    Spider[String]{res: Response => res.response.statusLine}
-      .withPipeline(HtmlSavePipeline[String]("/Users/admin/data/spider/fang/"))
-      .withStartUrl({
-        (1 to 51).map("http://shop.fang.com/zu/house/i3%d/".format(_)).map(Request(_))
-      }).start()
+    val houseListSpider = Spider[String](res => {
+      res.response.statusLine
+    }).withPipeline(HtmlSavePipeline[String]("/Users/admin/data/spider/fang/")((t, r) => {
+      r.request.url.substring(32, r.request.url.length - 1)
+    })).withStartUrl({
+      (1 to 51).map("http://shop.fang.com/zu/house/i3%d/".format(_)).map(Request(_))
+    })
+    houseListSpider.start()
   }
 }
