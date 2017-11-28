7c6ebd64591aa9e43e4a1fb12865f658724b0e85
sheepmen <747033643@qq.com>
2017-11-28 20:32:01 +0800

    dev: update fang spider for windows path

--- src/main/scala/example/FangSpider.scala
+++ src/main/scala/example/FangSpider.scala
@@ -1,5 +1,7 @@
 package example
 
+import java.io.File
+
 import http.{Request, Response}
 import pipeline.HtmlSavePipeline
 import spider.Spider
@@ -12,11 +14,15 @@ object FangSpider {
   def main(args: Array[String]): Unit = {
     val houseListSpider = Spider[String](res => {
       res.response.statusLine
-    }).withPipeline(HtmlSavePipeline[String]("/Users/admin/data/spider/fang/")((t, r) => {
-      r.request.url.substring(32, r.request.url.length - 1)
+    }).withPipeline(HtmlSavePipeline[String](saveFolder())((t, r) => {
+      r.request.url.substring(32, r.request.url.length - 1) + ".txt"
     })).withStartUrl({
       (1 to 51).map("http://shop.fang.com/zu/house/i3%d/".format(_)).map(Request(_))
     })
     houseListSpider.start()
+  }
+
+  def saveFolder() ={
+    Seq(System.getProperty("user.home"), "data", "spider", "fang").mkString(File.separator)
   }
 }
