773c9b787b1c8bd77649b64925d19ba9e74061b9
sheepmen <747033643@qq.com>
2017-11-30 08:22:25 +0800

    dev: update request with async

--- src/main/scala/http/Request.scala
+++ src/main/scala/http/Request.scala
@@ -6,20 +6,22 @@ import org.asynchttpclient
 import spider.Spider
 import org.asynchttpclient.Dsl.{asyncHttpClient, _}
 import util.HttpUtil
+import scala.collection.JavaConverters._
 
 case class Request(
                     url: String,
                     method: String = Method.GET,
-                    param: Map[String, String] = Map.empty,
-                    data: Map[String, String] = Map.empty
+                    param: Map[String, Seq[String]] = Map.empty,
+                    data: Map[String, Seq[String]] = Map.empty,
+                    header: Map[String, Seq[String]] = Map.empty
                   ) {
   override def equals(obj: scala.Any) = {
     obj match {
       case _obj: Request =>
         if (
           _obj.method.equals(this.method) ||
-          _obj.url.equals(this.url)
-        ){
+            _obj.url.equals(this.url)
+        ) {
           true
         } else {
           false
@@ -29,8 +31,21 @@ case class Request(
     }
   }
 
+  def headerAsJava = {
+    header.map(m => m._1 -> m._2.asJava).asJava
+  }
+
+  def dataAsJava = {
+    data.map(m => m._1 -> m._2.asJava).asJava
+  }
+
+  def paramAsJava = {
+    param.map(m => m._1 -> m._2.asJava).asJava
+  }
+
   /**
     * 执行请求
+    *
     * @return 返回Response对象
     */
   def execute(spider: Spider[_]): Response = execute(spider.requestConfig)
@@ -40,8 +55,14 @@ case class Request(
     while (error_count <= config.tryCount) {
       val client = asyncHttpClient()
       try {
-        val response: asynchttpclient.Response = client.prepareRequest(
-          get(this.url).setHeader("User-Agent",HttpUtil.randomUserAgent).build()
+        val response: asynchttpclient.Response  = client.prepareRequest(
+          request(this.method, this.url)
+            .setFormParams(this.dataAsJava)
+            .setHeaders(this.headerAsJava)
+            .setHeader("User-Agent", HttpUtil.randomUserAgent)
+            .setQueryParams(this.paramAsJava)
+            .setMethod(this.method)
+            .build()
         ).execute().get()
         val _res = Response(this, response)
         if (config.test_func(_res)) {
@@ -50,7 +71,7 @@ case class Request(
           throw new Exception("test function return false")
         }
       } catch {
-        case e:Exception =>
+        case e: Exception =>
           error_count += 1
           if (error_count > config.tryCount) {
             throw new Exception(s"try count is max -> ${config.tryCount}", e)
