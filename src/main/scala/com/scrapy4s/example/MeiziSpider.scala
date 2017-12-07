3c958dccdadfd85c7c2acf17997e278314b72a42
sheepmen <747033643@qq.com>
2017-12-07 15:53:48 +0800

    dev: update meizi spider demo

--- src/main/scala/com/scrapy4s/example/MeiziSpider.scala
+++ src/main/scala/com/scrapy4s/example/MeiziSpider.scala
@@ -1,9 +1,10 @@
 package com.scrapy4s.example
 
 import com.scrapy4s.http.Request
+import com.scrapy4s.manage.CmdManage
 import com.scrapy4s.pipeline.FileDumpPipeline
 import com.scrapy4s.spider.Spider
-import com.scrapy4s.util.{FileUtil, HashUtil}
+import com.scrapy4s.util.{FileUtil, HashUtil, ThreadUtil}
 
 /**
   * Created by sheep3 on 2017/11/30.
@@ -13,21 +14,26 @@ object MeiziSpider {
     /**
       * 下载器
       */
-    val downloader = Spider().pipe(
-      FileDumpPipeline(FileUtil.pathWithHome(Seq("data", "spider", "meizi")))(r => {
-        HashUtil.getHash(r.url) + ".jpg"
-      })
-    )
+    val downloader = Spider("downloader")
+      .setTimeOut(10 * 1000)
+      .pipe(
+        FileDumpPipeline(FileUtil.pathWithHome(Seq("data", "spider", "meizi")))(r => {
+          HashUtil.getHash(r.url) + ".jpg"
+        })
+      )
 
     /**
       * 详情页
       */
-    val detail = Spider().pipe(_.regex("""<img alt=".*?src="(.*?)" /><br />""").map(_ (0)).foreach(url => downloader.execute(Request(url))))
+    val detail = Spider("detail")
+      .pipe(_.regex("""<img alt=".*?src="(.*?)" /><br />""")
+        .map(_ (0))
+        .foreach(url => downloader.execute(Request(url))))
 
     /**
       * 列表页
       */
-    Spider().pipeForRequest(r => {
+    val list = Spider("list").pipeForRequest(r => {
       /**
         * 获取详情页
         */
@@ -41,7 +47,18 @@ object MeiziSpider {
       r.regex("""<li><a href='(.*?)'>""")
         .map(_ (0))
         .map(i => Request(s"http://www.meizitu.com/a/$i"))
-    }).setStartUrl(Request(s"http://www.meizitu.com/a/more_1.html"))
-      .run()
+    })
+      .setStartUrl(Request(s"http://www.meizitu.com/a/more_1.html"))
+
+    /**
+      * 注册爬虫，并启动
+      */
+    CmdManage()
+      .setThreadCount(ThreadUtil.core * 3)
+      .register(downloader)
+      .register(detail)
+      .register(list)
+      .start()
+
   }
 }
