b3a5da9f043d1a482069af75eedd8738031de8fb
sheepmen <747033643@qq.com>
2017-12-19 21:15:10 +0800

    dev: update manage with test function

--- src/main/scala/com/scrapy4s/manage/CmdManage.scala
+++ src/main/scala/com/scrapy4s/manage/CmdManage.scala
@@ -2,6 +2,7 @@ package com.scrapy4s.manage
 
 import java.util.concurrent.LinkedBlockingQueue
 
+import com.scrapy4s.http.Response
 import com.scrapy4s.http.proxy.ProxyResource
 import com.scrapy4s.spider.Spider
 import com.scrapy4s.thread.{DefaultThreadPool, ThreadPool}
@@ -14,26 +15,33 @@ case class CmdManage(
                       var history: Option[Boolean] = None,
                       var threadCount: Int = Runtime.getRuntime.availableProcessors() * 2,
                       var currentThreadPool: Option[ThreadPool] = None,
+                      var test_func: Option[Response => Boolean] = None,
                       var proxyResource: Option[ProxyResource] = None
                     ) extends Manage {
-
-  def setProxyResource(proxy: ProxyResource) = {
-    this.proxyResource = Some(proxy)
-    this
-  }
 
   def setThreadPool(tp: DefaultThreadPool) = {
     this.currentThreadPool = Option(tp)
     this
   }
 
+  def setThreadCount(count: Int) = {
+    this.threadCount = count
+    this
+  }
+
+
   def setHistory(h: Boolean) = {
     this.history = Option(h)
     this
   }
 
-  def setThreadCount(count: Int) = {
-    this.threadCount = count
+  def setProxyResource(proxy: ProxyResource) = {
+    this.proxyResource = Some(proxy)
+    this
+  }
+
+  def setTestFunc(newTestFunc: Response => Boolean) = {
+    this.test_func = Some(newTestFunc)
     this
   }
 
@@ -57,20 +65,20 @@ case class CmdManage(
 
   override def start(): Unit = {
     spiders
-      .map(_.setThreadPool(threadPool))
+      .foreach(s => {
+        s.setThreadPool(threadPool)
 
-    history match {
-      case Some(h) =>
-        spiders.map(_.setHistory(h))
-      case _ =>
-    }
+        if (test_func.isDefined) {
+          s.setTestFunc(test_func.get)
+        }
+        if (history.isDefined) {
+          s.setHistory(history.get)
+        }
+        if (proxyResource.isDefined) {
+          s.setProxyResource(proxyResource.get)
+        }
 
-    proxyResource match {
-      case Some(pr) =>
-        spiders
-          .map(_.setProxyResource(pr))
-      case _ =>
-    }
+      })
 
     spiders.foreach(_.start())
   }
